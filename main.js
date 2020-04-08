745d799122bf7b519860a2f58b4e63b72bfa7e6d
David Manthey <david.manthey@kitware.com>
2020-04-08 13:54:40 -0400

    Replace CSV parsing function to work in Firefox.

--- main.js
+++ main.js
@@ -375,27 +375,23 @@ function playAction(action) {
   }
 }
 
-/* https://github.com/peterthoeny/parse-csv-js, MIT License */
-function parseCsv(data, fieldSep, newLine) {
-  fieldSep = fieldSep || ',';
-  newLine = newLine || '\n';
-  var nSep = '\x1D';
-  var qSep = '\x1E';
-  var cSep = '\x1F';
-  var nSepRe = new RegExp(nSep, 'g');
-  var qSepRe = new RegExp(qSep, 'g');
-  var cSepRe = new RegExp(cSep, 'g');
-  var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\n]))"(|[\\s\\S]+?(?<![^"]"))"(?=($|[' + fieldSep + '\\n]))', 'g');
-  var grid = [];
-  data.replace(/\r/g, '').replace(/\n+$/, '').replace(fieldRe, function (match, p1, p2) {
-    return p2.replace(/\n/g, nSep).replace(/""/g, qSep).replace(/,/g, cSep);
-  }).split(/\n/).forEach(function (line) {
-    var row = line.split(fieldSep).map(function (cell) {
-      return cell.replace(nSepRe, newLine).replace(qSepRe, '"').replace(cSepRe, ',');
-    });
-    grid.push(row);
-  });
-  return grid;
+/* https://gist.github.com/Jezternz/c8e9fafc2c114e079829974e3764db75 */
+function parseCsv(strData) {
+  const objPattern = new RegExp('(\\,|\\r?\\n|\\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^\\,\\r\\n]*))', 'gi');
+  let arrMatches = null, arrData = [[]];
+  while (true) {
+    arrMatches = objPattern.exec(strData);
+    if (!arrMatches) {
+      break;
+    }
+    if (arrMatches[1].length && arrMatches[1] !== ',') {
+      arrData.push([]);
+    }
+    arrData[arrData.length - 1].push(arrMatches[2] ?
+      arrMatches[2].replace(new RegExp('""', 'g'), '"') :
+      arrMatches[3]);
+  }
+  return arrData;
 }
 // EOF
 
